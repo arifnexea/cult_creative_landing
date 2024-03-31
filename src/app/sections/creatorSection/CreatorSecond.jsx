ec29d501a36d4349e750e2dd76cfee3a5d6028fb
afiqnexea <afiqnrzm@hotmail.com>
2024-04-01 03:02:20 +0800

    A

--- src/app/sections/creatorSection/CreatorSecond.jsx
+++ src/app/sections/creatorSection/CreatorSecond.jsx
@@ -95,7 +95,7 @@ function CreatorSecond() {
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
-            src={vids[0].url}
+            src={vids[1].url}
             className="h-[27rem] rounded-lg"
             autoPlay
             loop
