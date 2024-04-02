a5e0caa86cbbeda583e48ae8eebc15b81b005011
mond1alaa111 <mond1alaa111@gmail.com>
2024-04-02 09:12:41 +0300

    new creator Form

--- src/app/sections/creatorSection/CreatorLast.jsx
+++ src/app/sections/creatorSection/CreatorLast.jsx
@@ -1,34 +1,42 @@
-'use client';
+"use client";
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import FormInput from "../../components/FormInput";
+import Link from "next/link";
+
 function CreatorLast() {
-const color = "#006D53"
+  const color = "#006D53";
 
   return (
     <section className="flex p-10  items-center justify-center bg-[#006D53]">
-    <div className="basis-1/2 hidden xl:block">
-      <Image
-        src={"https://storage.googleapis.com/landing_page_cult/creators/Cult%20Creative%202.jpg"}
-        alt="creator"
-        width={500}
-        height={500}
-        className="rounded-lg"
-      />
-    </div>
-    <div className="flex flex-col gap-5">
-      <div className="flex flex-col gap-5 ">
-        <h1 className="sm:text-8xl text-8xl -tracking-[.35rem]">
-          get  {" "}
-          <span className="font-serif italic -tracking-[.6rem] text-8xl">started.</span>
-        </h1>
+      <div className="basis-1/2 hidden xl:block">
+        <Image
+          src={
+            "https://storage.googleapis.com/landing_page_cult/creators/Cult%20Creative%202.jpg"
+          }
+          alt="creator"
+          width={500}
+          height={500}
+          className="rounded-lg"
+        />
       </div>
-      <div className="flex flex-col gap-6">
-        <h1 className="text-xl font-bold -tracking-[.05rem]">
-        We’re excited to work together. <br />Tell us who you are and we’ll get back to you with suitable brand deals.
-        </h1>
-        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 text-[#F4F4F4]">
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 ">
+          <h1 className="sm:text-8xl text-8xl -tracking-[.35rem]">
+            get{" "}
+            <span className="font-serif italic -tracking-[.6rem] text-8xl">
+              started.
+            </span>
+          </h1>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h1 className="text-xl font-bold -tracking-[.05rem]">
+            We’re excited to work together. <br />
+            Tell us who you are and we’ll get back to you with suitable brand
+            deals.
+          </h1>
+          {/* <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 text-[#F4F4F4]">
           <FormInput
             label={"Name"}
             name={"first-name"}
@@ -89,21 +97,25 @@ const color = "#006D53"
             type={"text"}
             color={color}
           />
-        </div>
-        <div className="mx-auto">
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.8 }}
-            className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
-          >
-            Submit
-          </motion.button>
+        </div> */}
+          <div className="mx-auto">
+            <Link
+            href={"/creators/contactForm"}
+            
+            >
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.8 }}
+              className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
+            >
+              Contact Us
+            </motion.button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-
-  </section>
-  )
+    </section>
+  );
 }
 
-export default CreatorLast
+export default CreatorLast;
