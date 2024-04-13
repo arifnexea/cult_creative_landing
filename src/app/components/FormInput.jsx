8b94dbcdc1423e75473f0ccb4f9d62904eae458c
afiqnexea <afiqnrzm@hotmail.com>
2024-04-14 01:57:26 +0800

    a

--- src/app/components/FormInput.jsx
+++ src/app/components/FormInput.jsx
@@ -1,12 +1,10 @@
 import React from "react";
+import Proptype from "prop-types";
 
-const FormInput = ({ label, name, type, color }) => {
+const FormInput = ({ label, name, type, color, field, errors }) => {
   return (
     <div className="sm:col-span-3">
-      <label
-        htmlFor="first-name"
-        className="block text-sm font-medium leading-6"
-      >
+      <label htmlFor={name} className="block text-sm font-medium leading-6">
         {label}
       </label>
       <div className="mt-2">
@@ -14,12 +12,27 @@ const FormInput = ({ label, name, type, color }) => {
           type={type}
           name={name}
           id={name}
+          {...field}
           autoComplete="given-name"
-          className={`block w-full rounded-full border-0 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-[${color}]`}
+          className={`block w-full rounded-full border-2 py-1.5 px-4 shadow-sm placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 bg-[${color}] ${
+            errors && "border-red-500"
+          }`}
         />
+        {errors && <p class="text-red-500 text-xs mx-2 my-1">{errors}</p>}
+        {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
       </div>
     </div>
   );
 };
 
 export default FormInput;
+
+FormInput.propTypes = {
+  label: Proptype.string,
+  name: Proptype.string,
+  type: Proptype.string,
+  color: Proptype.string,
+  onChange: Proptype.func,
+  placeholder: Proptype.string,
+  errors: Proptype.string,
+};
--- src/app/creators/contactForm/page.jsx
+++ src/app/creators/contactForm/page.jsx
@@ -1,14 +1,31 @@
 "use client";
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import Header from "@/app/components/Header";
 import LastSection from "@/app/sections/brandsection/LastSection";
-import { AnimatePresence, motion, wrap } from "framer-motion";
-import { Icon } from "@iconify/react";
+import { motion } from "framer-motion";
 import FormInput from "@/app/components/FormInput";
-import Link from "next/link";
 import Image from "next/image";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+import countries from "../../contants/countries.json";
+
 function CreatorForm() {
   const color = "#006D53";
+
+  const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email("Invalid email address").required(),
+    pronoun: yup.string().required(),
+    nationality: yup.string().required(),
+    birthDate: yup.string().required(),
+    location: yup.string().min(100, "Minumum 100 chars").required(),
+    instaUsername: yup.string().required(),
+    tiktokUsername: yup.string().required(),
+    language: yup.string().required(),
+    interests: yup.string().required(),
+  });
+
   return (
     <main className="flex min-h-screen flex-col">
       <Header logo={"/images/logo.svg"} />
@@ -40,109 +57,191 @@ function CreatorForm() {
                 Tell us who you are and we’ll get back to you with suitable
                 brand deals.
               </h1>
-              <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 text-[#F4F4F4]">
-                <FormInput
-                  label={"Full Name"}
-                  name={"first-name"}
-                  type={"text"}
-                  color={color}
-                />
-                <FormInput
-                  label={"Email Address"}
-                  name={"email-address"}
-                  type={"email"}
-                  color={color}
-                />
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="Pronoun"
-                    className="block text-sm font-medium leading-6"
-                  >
-                    Pronoun
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      className={`block w-full rounded-full border-0 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-[${color}]`}
-                    >
-                      <option value="he">He/Him </option>
-                      <option value="she">She/Her </option>
-                      <option value="they">They/Them</option>
-                      <option value="others">Others</option>
-                    </select>
+
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  pronoun: "",
+                  nationality: "",
+                  birthDate: "",
+                  location: "",
+                  instaUsername: "",
+                  tiktokUsername: "",
+                  language: "",
+                  interests: "",
+                }}
+                onSubmit={(values) => {
+                  alert(JSON.stringify(values, null, 2));
+                }}
+                validationSchema={schema}
+              >
+                <Form>
+                  <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 text-[#F4F4F4]">
+                    <Field name="name">
+                      {({ field, form: { errors } }) => (
+                        <>
+                          <FormInput
+                            field={field}
+                            type="text"
+                            label={"Name"}
+                            color={color}
+                            errors={errors.name}
+                          />
+                        </>
+                      )}
+                    </Field>
+                    <Field name="email">
+                      {({ field, form: { errors }, meta }) => (
+                        <FormInput
+                          field={field}
+                          label={"Email Address"}
+                          type={"email"}
+                          color={color}
+                          errors={errors.email}
+                        />
+                      )}
+                    </Field>
+
+                    <div className="sm:col-span-3">
+                      <Field name="pronoun">
+                        {({ field, form: { errors }, meta }) => (
+                          <>
+                            <label
+                              htmlFor="pronoun"
+                              className="block text-sm font-medium leading-6"
+                            >
+                              Pronoun
+                            </label>
+                            <div className="mt-2">
+                              <select
+                                name="pronoun"
+                                {...field}
+                                className={`block w-full rounded-full border-2 py-2 px-4 shadow-sm placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 bg-[${color}] ${
+                                  errors.pronoun && "border-red-500"
+                                }`}
+                              >
+                                <option value="">...</option>
+                                <option value="he">He/Him </option>
+                                <option value="she">She/Her </option>
+                                <option value="they">They/Them</option>
+                                <option value="others">Others</option>
+                              </select>
+                            </div>
+                            {errors.pronoun && (
+                              <p class="text-red-500 text-xs mx-2 my-1">
+                                {errors.pronoun}
+                              </p>
+                            )}
+                          </>
+                        )}
+                      </Field>
+                    </div>
+                    <div className="sm:col-span-3">
+                      <Field name="nationality">
+                        {({ field, form: { errors }, meta }) => (
+                          <>
+                            <label
+                              htmlFor="nationality"
+                              className="block text-sm font-medium leading-6"
+                            >
+                              Nationality
+                            </label>
+                            <div className="mt-2">
+                              <select
+                                {...field}
+                                className={`block w-full rounded-full border-2 py-2 px-4 shadow-sm placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 bg-[${color}] ${
+                                  errors.nationality && "border-red-500"
+                                }`}
+                              >
+                                <option value="">...</option>
+                                {countries.map((elem) => {
+                                  return (
+                                    <option key={elem.code} value={elem.name}>
+                                      {elem.name}
+                                    </option>
+                                  );
+                                })}
+                              </select>
+                            </div>
+                            {errors.nationality && (
+                              <p class="text-red-500 text-xs mx-2 my-1">
+                                {errors.nationality}
+                              </p>
+                            )}
+                          </>
+                        )}
+                      </Field>
+                    </div>
+                    <FormInput
+                      label={"Date of Birth"}
+                      name={"date-of-birth"}
+                      type={"date"}
+                      color={color}
+                    />
+                    <FormInput
+                      label={"Current Location"}
+                      name={"location"}
+                      type={"text"}
+                      color={color}
+                    />
+                    <FormInput
+                      label={"Instagram Username"}
+                      name={"instagram"}
+                      type={"text"}
+                      color={color}
+                    />
+                    <FormInput
+                      label={"Tik Tok Username"}
+                      name={"tiktok"}
+                      type={"text"}
+                      color={color}
+                    />
+                    <div className="sm:col-span-3">
+                      <label
+                        htmlFor="languages"
+                        className="block text-sm font-medium leading-6"
+                      >
+                        Languages
+                      </label>
+                      <div className="mt-2">
+                        <select
+                          value={["malay", "english"]}
+                          className={`block w-full rounded-full border-2 py-2 px-4 shadow-sm placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6  bg-[${color}]`}
+                        >
+                          <option value="english">English</option>
+                          <option value="malay">Malay </option>
+                          <option value="mandarin">Mandarin </option>
+                          <option value="tamil">Tamil </option>
+                          <option value="hindi">Hindi</option>
+                          <option value="others">Others</option>
+                        </select>
+                      </div>
+                    </div>
+                    <FormInput
+                      label={"Interests"}
+                      name={"interests"}
+                      type={"text"}
+                      color={color}
+                    />
                   </div>
-                </div>
-                <FormInput
-                  label={"Nationality"}
-                  name={"nationality"}
-                  type={"text"}
-                  color={color}
-                />
-                <FormInput
-                  label={"Date of Birth"}
-                  name={"date-of-birth"}
-                  type={"date"}
-                  color={color}
-                />
-                <FormInput
-                  label={"Current Location"}
-                  name={"location"}
-                  type={"text"}
-                  color={color}
-                />
-                <FormInput
-                  label={"Instagram Username"}
-                  name={"instagram"}
-                  type={"text"}
-                  color={color}
-                />
-                <FormInput
-                  label={"Tik Tok Username"}
-                  name={"tiktok"}
-                  type={"text"}
-                  color={color}
-                />
-                <div className="sm:col-span-3">
-                  <label
-                    htmlFor="languages"
-                    className="block text-sm font-medium leading-6"
-                  >
-                    Languages
-                  </label>
-                  <div className="mt-2">
-                    <select
-                      className={`block w-full rounded-full border-0 py-1.5 px-4 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-[${color}]`}
+                  <div className="mt-10 text-center">
+                    {/* <Link href={"/creators/contactForm"}> */}
+                    <motion.button
+                      whileHover={{ scale: 1.1 }}
+                      whileTap={{ scale: 0.8 }}
+                      type="submit"
+                      className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
                     >
-                      <option value="english">English </option>
-                      <option value="malay">Malay </option>
-                      <option value="mandarin">Mandarin </option>
-                      <option value="tamil">Tamil </option>
-                      <option value="hindi">Hindi</option>
-                      <option value="others">Others</option>
-                    </select>
+                      Submit
+                    </motion.button>
+                    {/* </Link> */}
                   </div>
-                </div>
-                <FormInput
-                  label={"Interests"}
-                  name={"interests"}
-                  type={"text"}
-                  color={color}
-                />
-              </div>
-              <div className="mx-auto">
-                <Link href={"/creators/contactForm"}>
-                  <motion.button
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.8 }}
-                    className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
-                  >
-                    Submit
-                  </motion.button>
-                </Link>
-              </div>
+                </Form>
+              </Formik>
             </div>
           </div>
         </section>
-
         <LastSection color={"#006D53"} />
       </Suspense>
     </main>
