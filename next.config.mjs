0ea7120bcaa6f5f4b7dfa72455372767aa90c51d
afiqnexea <afiqnrzm@hotmail.com>
2024-04-21 17:08:03 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["storage.googleapis.com"],
-    },
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
