db0baee9c48ccf7b03e40141314dba61fa91cd19
afiqnexea <afiqnrzm@hotmail.com>
2024-04-29 11:12:02 +0800

    a

--- src/app/sections/creatorSection/CreatorLast.jsx
+++ src/app/sections/creatorSection/CreatorLast.jsx
@@ -13,11 +13,11 @@ function CreatorLast() {
       <div className="basis-1/2 hidden xl:block">
         <Image
           src={
-            "https://storage.googleapis.com/landing_page_cult/creators/Cult%20Creative%202.jpg"
+            "https://storage.googleapis.com/landing-cultcreative/creator/Cult%20Creative%202%20(1).jpg"
           }
           alt="creator"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
           className="rounded-lg"
         />
       </div>
@@ -36,80 +36,15 @@ function CreatorLast() {
             Tell us who you are and we’ll get back to you with suitable brand
             deals.
           </h1>
-          {/* <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 text-[#F4F4F4]">
-          <FormInput
-            label={"Name"}
-            name={"first-name"}
-            type={"text"}
-            color={color}
-          />
-          <FormInput
-            label={"Email Address"}
-            name={"email-address"}
-            type={"email"}
-            color={color}
-          />
-          <FormInput
-            label={"Phone Number"}
-            name={"phone-number"}
-            type={"number"}
-            color={color}
-          />
-          <FormInput
-            label={"Nationality"}
-            name={"nationality"}
-            type={"text"}
-            color={color}
-          />
-          <FormInput
-            label={"Date of Birth"}
-            name={"date-of-birth"}
-            type={"date"}
-            color={color}
-          />
-          <FormInput
-            label={"Current Location"}
-            name={"location"}
-            type={"text"}
-            color={color}
-          />
-          <FormInput
-            label={"Instagram Username"}
-            name={"instagram"}
-            type={"text"}
-            color={color}
-          />
-          <FormInput
-            label={"Tik Tok Username"}
-            name={"tiktok"}
-            type={"text"}
-            color={color}
-          />
-          <FormInput
-            label={"Languages"}
-            name={"languages"}
-            type={"text"}
-            color={color}
-          />
-           <FormInput
-            label={"Interests"}
-            name={"interests"}
-            type={"text"}
-            color={color}
-          />
-        </div> */}
           <div className="mx-auto">
-            <Link
-            href={"/creators/contactForm"}
-            
-            >
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.8 }}
-              className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
-            >
-              Contact Us
-            </motion.button>
+            <Link href={"/creators/contactForm"}>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.8 }}
+                className={`bg-slate-100 py-2 px-12 rounded-full text-[${color}] font-serif uppercase flex-none`}
+              >
+                Contact Us
+              </motion.button>
             </Link>
           </div>
         </div>
