207dea0ab226540684e3cdc3781092a006920150
afiqnexea <afiqnrzm@hotmail.com>
2024-04-29 11:48:02 +0800

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,11 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link
-        rel="icon"
-        href="https://static.wixstatic.com/media/32f93d_e99c7f1506164a29a738412bee764493%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/32f93d_e99c7f1506164a29a738412bee764493%7Emv2.png"
-        type="image/png"
-      />
+      <Head>
+        <link
+          rel="icon"
+          href="https://static.wixstatic.com/media/32f93d_e99c7f1506164a29a738412bee764493%7Emv2.png/v1/fill/w_192%2Ch_192%2Clg_1%2Cusm_0.66_1.00_0.01/32f93d_e99c7f1506164a29a738412bee764493%7Emv2.png"
+          type="image/png"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
