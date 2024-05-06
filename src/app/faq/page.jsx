8cbb685cf84b658c8cadaca7d84c14f2cee720e7
afiqnexea <afiqnrzm@hotmail.com>
2024-05-06 15:27:44 +0800

    a

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -2,8 +2,44 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
 import LastSection from "../sections/brandsection/LastSection";
+import questions from "../contants/general.json";
 
 const Accordion = () => {
+  const [currentTabs, setCurrentTab] = useState("general");
+
+  const handleChangeTab = (e) => {
+    setCurrentTab(e.target.value);
+  };
+
+  const general = questions.map((item, i) => (
+    <div key={i} className="flex flex-col text-lg">
+      <Item title={item.question} text={item.answer} />
+    </div>
+  ));
+
+  const brand = (
+    <div className="flex flex-col text-lg">
+      <h1>None</h1>
+    </div>
+  );
+
+  const creator = (
+    <div className="flex flex-col text-lg">
+      <Item
+        title={"Do I have to pay to use Cult Creative?"}
+        text={
+          "Nope! Cult Creative is a free platform and won't cost you anything. "
+        }
+      />
+      <Item
+        title={"Who do I contact if I have any questions/inquiries?"}
+        text={
+          "You may email our support team at support@cultcreative.asia or DM us on Instagram at @cultcreativeasia"
+        }
+      />
+    </div>
+  );
+
   return (
     <>
       <Header />
@@ -26,46 +62,44 @@ const Accordion = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 px-5">
-            <AccordionItem
-              header="Do I have to pay to use Cult Creative?"
-              text="Nope! Cult Creative is a free platform and won’t cost you anything. "
-            />
-            <AccordionItem
-              header="Who do I contact if I have any questions/inquiries?"
-              text="You may email our support team at support@cultcreative.asia or DM us on Instagram at @cultcreativeasia"
-            />
+          {/* Tabs */}
+          <div className="flex flex-row gap-5 px-5 my-10 font-aileron font-bold">
+            <button
+              value={"general"}
+              className={`${
+                currentTabs === "general" &&
+                "border-b-4 border-b-indigo-500 rounded-sm transition ease-in-out duration-300"
+              }`}
+              onClick={handleChangeTab}
+            >
+              General
+            </button>
+            <button
+              value={"brand"}
+              className={`${
+                currentTabs === "brand" &&
+                "border-b-4 border-b-indigo-500 rounded-sm  transition ease-in-out duration-300"
+              }`}
+              onClick={handleChangeTab}
+            >
+              Brand
+            </button>
+            <button
+              value={"creator"}
+              className={`${
+                currentTabs === "creator" &&
+                "border-b-4 border-b-indigo-500 rounded-sm transition ease-in-out duration-300"
+              }`}
+              onClick={handleChangeTab}
+            >
+              Creator
+            </button>
           </div>
-        </div>
 
-        <div className="absolute bottom-0 right-0 z-[-1]">
-          <svg
-            width="1440"
-            height="886"
-            viewBox="0 0 1440 886"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              opacity="0.5"
-              d="M193.307 -273.321L1480.87 1014.24L1121.85 1373.26C1121.85 1373.26 731.745 983.231 478.513 729.927C225.976 477.317 -165.714 85.6993 -165.714 85.6993L193.307 -273.321Z"
-              fill="url(#paint0_linear)"
-            />
-            <defs>
-              <linearGradient
-                id="paint0_linear"
-                x1="1308.65"
-                y1="1142.58"
-                x2="602.827"
-                y2="-418.681"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#3056D3" stop-opacity="0.36" />
-                <stop offset="1" stop-color="#F5F2FD" stop-opacity="0" />
-                <stop offset="1" stop-color="#F5F2FD" stop-opacity="0.096144" />
-              </linearGradient>
-            </defs>
-          </svg>
+          {/* Items */}
+          {currentTabs === "general" && general}
+          {currentTabs === "brand" && brand}
+          {currentTabs === "creator" && creator}
         </div>
       </section>
       <LastSection />
@@ -75,51 +109,64 @@ const Accordion = () => {
 
 export default Accordion;
 
-const AccordionItem = ({ header, text }) => {
-  const [active, setActive] = useState(false);
+// const AccordionItem = ({ header, text }) => {
+//   const [active, setActive] = useState(false);
 
-  const handleToggle = () => {
-    event.preventDefault();
-    setActive(!active);
-  };
+//   const handleToggle = (event) => {
+//     event.preventDefault();
+//     setActive(!active);
+//   };
+
+//   return (
+//     <div className="mb-8 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)] dark:bg-dark-2 dark:shadow-[0px_20px_95px_0px_rgba(0,0,0,0.30)] sm:p-8 lg:px-6 xl:px-8 text-black">
+//       <button
+//         className={`faq-btn flex w-full text-left`}
+//         onClick={handleToggle}
+//       >
+//         <div className="mr-5 flex h-10 w-full max-w-[40px] items-center justify-center rounded-lg bg-primary/5 text-primary dark:bg-white/5">
+//           <svg
+//             className={`fill-primary stroke-primary duration-200 ease-in-out ${
+//               active ? "rotate-180" : ""
+//             }`}
+//             width="17"
+//             height="10"
+//             viewBox="0 0 17 10"
+//             xmlns="http://www.w3.org/2000/svg"
+//           >
+//             <path
+//               d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
+//               fill=""
+//               stroke=""
+//             />
+//           </svg>
+//         </div>
+
+//         <div className="w-full">
+//           <h4 className="mt-1 text-lg font-semibold">{header}</h4>
+//         </div>
+//       </button>
+
+//       <div
+//         className={`pl-[62px] duration-200 ease-in-out ${
+//           active ? "block" : "hidden"
+//         }`}
+//       >
+//         <p className="py-3 text-base leading-relaxed text-body-color dark:text-dark-6">
+//           {text}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+const Item = ({ title, text, position }) => {
+  let defaultCSS =
+    "grid grid-cols-1 md:grid-cols-2 gap-5 px-5 py-10 border-t-2 border-t-gray-200";
+
   return (
-    <div className="mb-8 w-full rounded-lg bg-white p-4 shadow-[0px_20px_95px_0px_rgba(201,203,204,0.30)] dark:bg-dark-2 dark:shadow-[0px_20px_95px_0px_rgba(0,0,0,0.30)] sm:p-8 lg:px-6 xl:px-8 text-black">
-      <button
-        className={`faq-btn flex w-full text-left`}
-        onClick={() => handleToggle()}
-      >
-        <div className="mr-5 flex h-10 w-full max-w-[40px] items-center justify-center rounded-lg bg-primary/5 text-primary dark:bg-white/5">
-          <svg
-            className={`fill-primary stroke-primary duration-200 ease-in-out ${
-              active ? "rotate-180" : ""
-            }`}
-            width="17"
-            height="10"
-            viewBox="0 0 17 10"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-              fill=""
-              stroke=""
-            />
-          </svg>
-        </div>
-
-        <div className="w-full">
-          <h4 className="mt-1 text-lg font-semibold">{header}</h4>
-        </div>
-      </button>
-
-      <div
-        className={`pl-[62px] duration-200 ease-in-out ${
-          active ? "block" : "hidden"
-        }`}
-      >
-        <p className="py-3 text-base leading-relaxed text-body-color dark:text-dark-6">
-          {text}
-        </p>
-      </div>
+    <div className={defaultCSS}>
+      <p className="font-bold text-gray-800">{title}</p>
+      <p className="text-gray-600">{text}</p>
     </div>
   );
 };
