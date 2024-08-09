d433fc961d7018001aa98ef899da48b7161536e0
afiqnexea <afiqnrzm@hotmail.com>
2024-08-09 11:53:31 +0800

    a

--- src/app/sections/creatorSection/CreatorFirst.jsx
+++ src/app/sections/creatorSection/CreatorFirst.jsx
@@ -51,7 +51,7 @@ const CreatorFirst = () => {
               lineHeight: "85%",
             }}
           >
-            get <span className="font-times">brandsssss</span>
+            get <span className="font-times">brands</span>
             <br />
             <span className="font-times italic -tracking-[.5rem] xl:-tracking-[.8rem]">
               deals,{" "}
